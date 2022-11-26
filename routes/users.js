fcdd07ac2fcc34ee8a5d2b097886b6fcf4b0bafd
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2022-11-26 11:22:49 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello Kitty');
+  res.status(200).json({fullname: 'Kanlayanuch Srichote'})
 });
+router.get('/bio', function(req, res, next) {
+  res.status(200).json({
+    fullname: 'Kanlayanuch Srichote',
+    nickname: 'Kat',
+    hobby: 'sleep',
+    gitusername: 'KSdesu06'
+  })
+});
+
 
 module.exports = router;
